25313fa9c18537158a5031429311fef91537d0ae
Редько Никита <90651865+sapp0612@users.noreply.github.com>
2021-12-18 22:51:45 +0300

    Update n_prost.py

--- n_prost.py
+++ n_prost.py
@@ -1,10 +1,10 @@
 def prime(number):
     if number in [1, 2, 3]:
-        return 'Простое'
+        return True
     sqrt_number = int(number ** 0.5)
     for el in range(2, sqrt_number + 1):
         if number % el == 0:
-            return 'Составное'
-    return 'простое'
+            return False
+    return True 
     
-print(prime(111))
+print(prime(3571))
